7b7e103baffceea14e7228816e54c41d505c3aaa
zhangx1923 <xz8@email.sc.edu>
2021-11-30 01:07:11 -0500

    update

--- coco_eval.py
+++ coco_eval.py
@@ -27,13 +27,11 @@ class CocoEvaluator:
     def update(self, predictions):
         img_ids = list(np.unique(list(predictions.keys())))
         self.img_ids.extend(img_ids)
-
         for iou_type in self.iou_types:
             results = self.prepare(predictions, iou_type)
             with redirect_stdout(io.StringIO()):
                 coco_dt = COCO.loadRes(self.coco_gt, results) if results else COCO()
             coco_eval = self.coco_eval[iou_type]
-
             coco_eval.cocoDt = coco_dt
             coco_eval.params.imgIds = list(img_ids)
             img_ids, eval_imgs = evaluate(coco_eval)
